1a7cfd4017f6adf019905ee912e63654317c487a
unknown <eduardomatheusuesc@gmail.com>
2023-11-13 12:01:20 -0300

    Continuando exemplo da aula Rede_social

--- aula02/Rede_social/src/cliente/Cliente.java
+++ aula02/Rede_social/src/cliente/Cliente.java
@@ -1,9 +1,11 @@
 package cliente;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Cliente {
 	private String nome, email, nacionalidade;
+	private int quantidadeDePostagens, pontuacao;
 	private ArrayList<String> postagens;
 	
 	public String getNome() {
@@ -31,14 +33,64 @@ public class Cliente {
 		this.postagens = postagens;
 	}
 	
-	public Cliente(String nome, String email, String nacionalidade, ArrayList<String> postagens) {
+	public void adicionaPostagens(String postagem) {
+		this.quantidadeDePostagens++;
+		postagens.add(postagem);
+	}
+	
+	public void listaPostagens() {
+		for (String post : postagens) {
+			System.out.println(post);
+		}
+	}
+	
+	public void alteraPontuacao(int delta) {
+		pontuacao+=delta;
+	}
+	
+	public Cliente(String nome, String email, String nacionalidade) {
 		super();
 		this.nome = nome;
 		this.email = email;
 		this.nacionalidade = nacionalidade;
-		this.postagens = postagens;
+		this.quantidadeDePostagens = 0;
+		this.pontuacao = 0;
+		this.postagens = new ArrayList<String>();
 	}
 	
+	public static void main(String[] args) {
+		String nome, email, nacionalidade, postagem;
+		Cliente cliente;
+		
+		System.out.println("Informe seu nome: ");
+		Scanner sc = new Scanner(System.in);
+		nome = sc.nextLine();
+		
+		System.out.println("Informe seu email: ");
+		email = sc.nextLine();
+		
+		System.out.println("Informe sua nacionalidade: ");
+		nacionalidade = sc.nextLine();
+		
+		System.out.println("Escreva uma postagem: ");
+		postagem = sc.nextLine();
+		
+		cliente = new Cliente(nome, email, nacionalidade);
+		
+		cliente.adicionaPostagens(postagem);
+		
+		System.out.println("Escreva mais uma postagem: ");
+		postagem = sc.nextLine();
+		
+		cliente.adicionaPostagens(postagem);
+		
+		System.out.println("Escreva mais uma postagem: ");
+		postagem = sc.nextLine();
+		
+		cliente.adicionaPostagens(postagem);
+		
+		cliente.listaPostagens();
+	}
 	
 	
 	
